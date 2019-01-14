97a2cd8afad755acde19aa69ed323531944e855a
Remi Thebault <remi.thebault@gmail.com>
2019-01-14 01:50:30 +0100

    hello

--- examples/hello.rs
+++ examples/hello.rs
@@ -2,10 +2,13 @@
 extern crate ows;
 #[cfg(unix)]
 use ows::display::wayland as disp;
+
+use ows::display::Display;
 use ows::window::Window;
 
 fn main() {
-    let mut dpy = disp::Display::open().expect("could not open display");
-    let mut win = Window::new(&mut dpy);
+    let dpy = disp::Display::open().expect("could not open display");
+    let mut win = dpy.create_window();
+
     win.set_title(String::from("Hello, Ows!"))
 }
