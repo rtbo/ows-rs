713f0da0f7612c2b08706f96e4552c6dd17bd225
Remi Thebault <remi.thebault@gmail.com>
2019-01-18 22:40:03 +0100

    rustfmt

--- src/display/wayland/mod.rs
+++ src/display/wayland/mod.rs
@@ -1,84 +1,74 @@
-
-mod window;
-
-use self::window::Window;
-
-use wlc::ConnectError;
-use wlc::protocol::wl_compositor::{WlCompositor};
-use wlp::xdg_shell::client::xdg_wm_base::{self, XdgWmBase, RequestsTrait as XdgWmReqs};
 use std::cell::RefCell;
 use std::rc::Rc;
+use wlc::protocol::wl_compositor::WlCompositor;
+use wlc::ConnectError;
+use wlp::xdg_shell::client::xdg_wm_base::{self, RequestsTrait as XdgWmReqs, XdgWmBase};
 
-pub struct Display
-{
+mod window;
+use self::window::Window;
+
+pub struct Display {
     shared: Rc<RefCell<DisplayShared>>,
 }
 
-impl Display
-{
-}
+impl Display {}
 
-impl Drop for Display
-{
+impl Drop for Display {
     fn drop(&mut self) {}
 }
 
-impl super::Display for Display
-{
+impl super::Display for Display {
     type Window = Window;
     type OpenError = ConnectError;
 
-    fn open() -> Result<Display, ConnectError>
-    {
-        wlc::Display::connect_to_env()
-            .map(|(dpy, queue)| Display {
-                shared: DisplayShared::new(dpy, queue)
-            })
+    fn open() -> Result<Display, ConnectError> {
+        wlc::Display::connect_to_env().map(|(dpy, queue)| Display {
+            shared: DisplayShared::new(dpy, queue),
+        })
     }
 
-    fn create_window(&self) -> Window
-    {
+    fn create_window(&self) -> Window {
         Window::new(self.shared.clone())
     }
 
     fn collect_events(&self) {}
 }
 
-
-struct DisplayShared
-{
+struct DisplayShared {
     _dpy: wlc::Display,
     queue: wlc::EventQueue,
     compositor: wlc::Proxy<WlCompositor>,
     xdg_shell: wlc::Proxy<XdgWmBase>,
 }
 
-impl DisplayShared
-{
-    fn new (dpy: wlc::Display, mut queue: wlc::EventQueue) -> Rc<RefCell<Self>>
-    {
+impl DisplayShared {
+    fn new(dpy: wlc::Display, mut queue: wlc::EventQueue) -> Rc<RefCell<Self>> {
         use wlc::GlobalManager;
 
         let globals = GlobalManager::new(&dpy);
         queue.sync_roundtrip().unwrap();
 
-        let compositor = globals.instantiate_auto::<WlCompositor, _>(
-            |np| np.implement(|_, _| {}, ())
-        ).unwrap();
-        let xdg_shell = globals.instantiate_auto::<XdgWmBase, _>(
-            |np| np.implement(|ev, xdg_shell| {
-                match ev {
-                    xdg_wm_base::Event::Ping{serial} => xdg_shell.pong(serial)
-                }
-            }, ())
-        ).unwrap();
+        let compositor = globals
+            .instantiate_auto::<WlCompositor, _>(|np| np.implement(|_, _| {}, ()))
+            .unwrap();
+        let xdg_shell = globals
+            .instantiate_auto::<XdgWmBase, _>(|np| {
+                np.implement(
+                    |ev, xdg_shell| match ev {
+                        xdg_wm_base::Event::Ping { serial } => xdg_shell.pong(serial),
+                    },
+                    (),
+                )
+            })
+            .unwrap();
 
         for (id, interface, version) in globals.list() {
             println!("{}: {} (version = {})", id, interface, version);
         }
 
-        Rc::new(RefCell::new(Self{
-            _dpy: dpy, queue: queue,
+        Rc::new(RefCell::new(Self {
+            _dpy: dpy,
+            queue: queue,
             compositor: compositor,
             xdg_shell: xdg_shell,
         }))
--- src/display/wayland/window.rs
+++ src/display/wayland/window.rs
@@ -1,52 +1,47 @@
-
-use super::{DisplayShared, Display};
+use super::{Display, DisplayShared};
 use crate::window;
-use wlc::protocol::wl_compositor::RequestsTrait as CompReqs;
-use wlc::protocol::wl_surface::WlSurface;
-use wlp::xdg_shell::client::xdg_wm_base::RequestsTrait as XdgWmReqs;
-use wlp::xdg_shell::client::xdg_surface::{self, XdgSurface, RequestsTrait as XdgSurfReqs};
-use wlp::xdg_shell::client::xdg_toplevel::{self, XdgToplevel, RequestsTrait as XdgTlReqs};
 use std::cell::RefCell;
 use std::rc::Rc;
+use wlc::protocol::wl_compositor::RequestsTrait as CompReqs;
+use wlc::protocol::wl_surface::WlSurface;
+use wlp::xdg_shell::client::xdg_surface::{self, RequestsTrait as XdgSurfReqs, XdgSurface};
+use wlp::xdg_shell::client::xdg_toplevel::{self, RequestsTrait as XdgTlReqs, XdgToplevel};
+use wlp::xdg_shell::client::xdg_wm_base::RequestsTrait as XdgWmReqs;
 
-
-pub struct Window
-{
+pub struct Window {
     disp_shared: Rc<RefCell<DisplayShared>>,
     shared: Rc<RefCell<WindowShared>>,
     title: String,
 }
 
-struct WindowShared
-{
+struct WindowShared {
     _surf: Option<wlc::Proxy<WlSurface>>,
     _xdg_surf: Option<wlc::Proxy<XdgSurface>>,
     xdg_tl: Option<wlc::Proxy<XdgToplevel>>,
     size: (i32, i32),
 }
 
-impl Window
-{
-    pub(in super) fn new(shared: Rc<RefCell<DisplayShared>>) -> Window {
+impl Window {
+    pub(super) fn new(shared: Rc<RefCell<DisplayShared>>) -> Window {
         Window {
             disp_shared: shared.clone(),
             shared: Rc::new(RefCell::new(WindowShared {
-                _surf: None, _xdg_surf: None, xdg_tl: None,
-                size: (0, 0)
+                _surf: None,
+                _xdg_surf: None,
+                xdg_tl: None,
+                size: (0, 0),
             })),
             title: String::new(),
         }
     }
 }
 
-impl window::Window<Display> for Window
-{
+impl window::Window<Display> for Window {
     fn title(&self) -> &str {
         &self.title
     }
 
-    fn set_title(&mut self, val: String)
-    {
+    fn set_title(&mut self, val: String) {
         match &self.shared.borrow_mut().xdg_tl {
             Some(tl) => tl.set_title(val.clone()),
             _ => {}
@@ -54,35 +49,47 @@ impl window::Window<Display> for Window
         self.title = val;
     }
 
-    fn show (&mut self, _: window::State)
-    {
+    fn show(&mut self, _: window::State) {
         let disp_shared = self.disp_shared.borrow();
 
-        let surf = disp_shared.compositor.create_surface(
-            |np| np.implement(|_, _| {}, ())
-        ).unwrap();
+        let surf = disp_shared
+            .compositor
+            .create_surface(|np| np.implement(|_, _| {}, ()))
+            .unwrap();
 
-        let xdg_surf = disp_shared.xdg_shell.get_xdg_surface(
-            &surf, |np| np.implement(|ev, xdg_surf| {
-                match ev {
-                    xdg_surface::Event::Configure{serial} => xdg_surf.ack_configure(serial)
-                }
-            }, ())
-        ).unwrap();
+        let xdg_surf = disp_shared
+            .xdg_shell
+            .get_xdg_surface(&surf, |np| {
+                np.implement(
+                    |ev, xdg_surf| match ev {
+                        xdg_surface::Event::Configure { serial } => xdg_surf.ack_configure(serial),
+                    },
+                    (),
+                )
+            })
+            .unwrap();
 
         let ws = self.shared.clone();
-        let xdg_tl = unsafe { xdg_surf.get_toplevel(
-            |np| np.implement_nonsend(move |ev, _| {
-                match ev {
-                    xdg_toplevel::Event::Configure{width, height, states: _} => {
-                        ws.borrow_mut().size = (width, height);
-                    },
-                    xdg_toplevel::Event::Close => {
-
-                    }
-                }
-            }, (), &disp_shared.queue.get_token())
-        ).unwrap() };
+        let xdg_tl = unsafe {
+            xdg_surf
+                .get_toplevel(|np| {
+                    np.implement_nonsend(
+                        move |ev, _| match ev {
+                            xdg_toplevel::Event::Configure {
+                                width,
+                                height,
+                                states: _,
+                            } => {
+                                ws.borrow_mut().size = (width, height);
+                            }
+                            xdg_toplevel::Event::Close => {}
+                        },
+                        (),
+                        &disp_shared.queue.get_token(),
+                    )
+                })
+                .unwrap()
+        };
 
         xdg_tl.set_title(self.title.clone());
 
